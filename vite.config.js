93b68d5cff0136f3f41918eb1399108413cb4ae3
WyattBensman <wyattbensman5@gmail.com>
2024-01-10 15:41:58 -0500

    Help

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 4173,
+    port: process.env.PORT || 3000,
   },
 });
